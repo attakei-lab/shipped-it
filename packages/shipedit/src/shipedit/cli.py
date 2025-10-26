c01143d8859156f40f7d66c272330a5501f1bd33
Kazuya Takei <myself@attakei.net>
2025-10-27 03:31:14 +0900

    feat: Simple entrypoint

--- packages/shipedit/src/shipedit/cli.py
+++ packages/shipedit/src/shipedit/cli.py
@@ -1,2 +1,25 @@
+import logging
+
+from pydantic import ValidationError
+from pydantic_settings import BaseSettings, CliApp, CliPositionalArg, SettingsConfigDict
+
+
+class CliSettings(BaseSettings):
+    model_config = SettingsConfigDict(cli_parse_args=True)
+
+    package: CliPositionalArg[str]
+    """Package type of that shiped."""
+    name: CliPositionalArg[str]
+    """Name of target package."""
+
+
 def main() -> int:
+    logging.basicConfig(level=logging.INFO)
+    try:
+        args = CliApp.run(CliSettings)
+        logging.debug("Arguments: %s %s", args.package, args.name)
+    except ValidationError as e:
+        print(f"Error: {e}")
+        return 1
+
     return 0
