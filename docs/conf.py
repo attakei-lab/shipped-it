9b9927f97657b4a3a33084cc0c47748c3e87175a
Kazuya Takei <myself@attakei.net>
2025-11-11 02:24:31 +0900

    docs: Initial commit

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+# -- Project information
+project = "Shipped-it"
+copyright = "2025, Kazuya Takei"
+author = "Kazuya Takei"
+release = "2025.11"
+
+# -- General configuration
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
