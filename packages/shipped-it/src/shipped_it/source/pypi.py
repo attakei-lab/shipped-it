3c85e53a64b4d19d25628e37fd6e39da3a74ba48
Kazuya Takei <myself@attakei.net>
2025-11-13 05:17:07 +0900

    feat(pypi): Post text can include relative release date.

--- packages/shipped-it/src/shipped_it/source/pypi.py
+++ packages/shipped-it/src/shipped_it/source/pypi.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 from functools import lru_cache
 from typing import Any, TypedDict, override
 
 import requests
+from dateutil.parser import isoparse
+from dateutil.relativedelta import relativedelta
+from dateutil.tz import tzlocal
 from pydantic import BaseModel, HttpUrl
 
 from .. import models
@@ -22,12 +26,15 @@ class Source(models.Source[Options]):
         data_url = f"https://pypi.org/pypi/{release_name}/json"
         data = fetch_project_data(data_url)
 
+        release_date = find_release_date(data.releases, data.info.version)
         release = models.Release(
             source=self,  # type: ignore[invalid-argument-type]
             name=data.info.name,
             revision=data.info.version,
             extra={
                 "github": find_github_data(data.info),
+                "release_date": release_date,
+                "release_date_text": make_release_date_text(release_date),
             },
         )
         return release
@@ -44,6 +51,7 @@ class ProjectInfo(BaseModel):
 
 class ProjectFileinfo(BaseModel):
     filename: str
+    upload_time_iso_8601: str
 
 
 class ProjectData(BaseModel):
@@ -71,3 +79,33 @@ def find_github_data(info: ProjectInfo) -> dict[str, Any] | None:
     return {
         "url": github_url,
     }
+
+
+def find_release_date(
+    releases: dict[str, list[ProjectFileinfo]], version: str
+) -> datetime | None:
+    if version not in releases:
+        return None
+    latest_uploaded = max([f.upload_time_iso_8601 for f in releases[version]])
+    return isoparse(latest_uploaded)
+
+
+def make_release_date_text(release_date: datetime | None) -> str | None:
+    if not release_date:
+        return None
+    delta = relativedelta(datetime.now(tz=tzlocal()), release_date)
+    match delta.days:
+        case 0:
+            if delta.hours <= 1:
+                return "Now"
+            elif delta.hours <= 6:
+                return f"{delta.hours} hours ago"
+            else:
+                return "Today"
+        case 1:
+            return "Yesterday"
+        case _:
+            if delta.years + delta.months == 0 and delta.days <= 20:
+                return f"{delta.days} days ago"
+            else:
+                return "Previously"
